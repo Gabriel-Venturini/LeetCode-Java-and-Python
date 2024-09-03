f86c3f223b119cd129f9891f7c2018666e9624c4
Gabriel Venturini <dev.venturini@gmail.com>
2024-09-03 19:43:03 -0300

    release: 01. Two Sum

---
+++ 01. Two Sum/TwoSum.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class TwoSum {
+
+    public static void main(String[] args) {
+        int[] numbers = {3,3};
+        int tgt = 6;
+        int[] result = twoSum(numbers, tgt);
+        
+        System.out.println(Arrays.toString(result));
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        int result[] = new int[2]; // positions i and j
+        boolean foundIt = false;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    foundIt = true;
+                    break;
+                }
+                }
+            if (foundIt) {
+                break;
+            }
+        }
+        return result;
+    }
+}
